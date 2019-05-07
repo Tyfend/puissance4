a1f55b317a80ba8904b6db97d58cf66575bf954f
Tyfend <morvantifenn@gmail.com>
2019-05-07 12:00:26 +0200

    js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var numeroJoueur = 1;
+var nbrColonnes = 5;
+var	nbrLignes = 5;
+var jeu = true;
+var	texte = ;
+var plateauJeu = [];
+
+// boucle
+// nrb de tour sera nrb de lignes dans le jeu
+// a chaque tour :
+// dans le tableau plateau -> nouveau tableau avec index numéro du tour
+
+for (var i = 0; i =< nbrLignes ; i++) {
+	plateauJeu = [$i][];
+}
